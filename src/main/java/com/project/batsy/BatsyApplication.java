5033023e410942f80aaa0dcf7ac114789fe88cd5
saurabh-singh-thakur91 <saurabh.singh.thakur91@gmail.com>
2019-09-29 18:39:03 +0530

    Started project Batsy, using Spring boot

---
+++ src/main/java/com/project/batsy/BatsyApplication.java
@@ -0,0 +1,13 @@
+package com.project.batsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatsyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/batsy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project.batsy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BatsyApplication.class);
+	}
+
+}
